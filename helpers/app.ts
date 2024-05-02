57dd871099b8c1f16a2e47d2dbf557b63789a82d
Christophe 'DrakeXorn' Bernard <chbernard4444@hotmail.com>
2024-05-02 12:31:42 +0200

    refactor(QrScan): Remove unnecessary throws

--- helpers/app.ts
+++ helpers/app.ts
@@ -8,9 +8,10 @@ const gamesToSlugs = {
 export function verifyQrCode(
 	code: string,
 ): { path: string; quest: number } | null {
+	let qrData;
 	try {
 		const value = atob(code);
-		const { game, quest, path } = z
+		qrData = z
 			.object({
 				game: z.string().min(1),
 				quest: z.number().min(1).max(3),
@@ -20,22 +21,24 @@ export function verifyQrCode(
 					.regex(/\/quests\/\d/),
 			})
 			.parse(JSON.parse(value));
-
-		const slug = gamesToSlugs[game as keyof typeof gamesToSlugs];
-
-		if (!slug) {
-			throw new Error("Invalid game");
-		}
-
-		const checkPath = `${slug}/quests/${quest}`;
-
-		if (path !== checkPath) {
-			throw new Error("Invalid path");
-		}
-
-		return { path: path.slice(slug.length), quest };
 	} catch (error) {
 		console.log("Invalid QR code", error);
 		return null;
 	}
+
+	const { game, quest, path } = qrData;
+
+	const slug = gamesToSlugs[game as keyof typeof gamesToSlugs];
+
+	if (!slug) {
+		return null;
+	}
+
+	const checkPath = `${slug}/quests/${quest}`;
+
+	if (path !== checkPath) {
+		return null;
+	}
+
+	return { path: path.slice(slug.length), quest };
 }
